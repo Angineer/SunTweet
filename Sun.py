1c99d2534f29e496c9a7e83bbfee8d2549ef6587
Andy Tracy <Poppyseedling@gmail.com>
2014-02-22 09:51:13 -0700

    Finished up the datum class, changed formatting on raw data stream a little bit

--- Sun.py
+++ Sun.py
@@ -4,20 +4,35 @@ class datum:
 
 		Arguments
 		path: The location of the data stream from which to read.
-			Should be a csv document with the following format:
-				timestamp, datavalue
+			Should be a text file with the following format:
+				date time datavalue
 			on each line
 		"""
 		self.dataSource=dataSource
+		self.date="01/01/2000"
+		self.time="00:00"
 		self.value=0
 
 	def Update(self):
 		"""Update the datapoint to have the most recent value from the data stream"""
 		#Read in raw data
-		new=0
+		with open(self.dataSource, 'r') as dataFile:
+			dataFile.seek(-22, 2)
+			line=dataFile.read(22)
+		
+		#Parse data
+		lineList=line.split()
+		date=lineList[0]
+		time=lineList[1]
+		value=lineList[2]
 
 		#Set to new value
-		self.value=new
+		self.date=date
+		self.time=time
+		self.value=value
+
+	def GetDateTime(self):
+		return self.date+" "+self.time
 
 	def GetValue(self):
-		retun self.value
+		return self.value
